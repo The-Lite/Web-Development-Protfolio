5878f4ff594f496903c59ac576796afd85ef1cde
Lite <35774598+The-Lite@users.noreply.github.com>
2021-12-15 16:10:43 +0100

    adding navbar 1.0

--- Django-rest-svelte/frontend/car2shop/src/routes/index.svelte
+++ Django-rest-svelte/frontend/car2shop/src/routes/index.svelte
@@ -1,11 +1,64 @@
-<div class="flex flex-row">
-    <div class="basis-1/4">01</div>
-    <div class="basis-1/4">02</div>
-    <div class="basis-1/2">03</div>
-  </div>
+<div class="nav flex flex-row" style="background-color: rebeccapurple;">
+    <div class="basis-1/2 logo"> 
+        <img src="" alt="logo"> </div>
+    <div class="left-nav flex flex-row basis-1/2 flex-nowrap items-center justify-end">
+       
+    <a href="" class="element basis-1/5">Home</a>
+    <a href="" class="element basis-1/5">Home</a>
+    <a href="" class="element basis-1/5">Home</a>
+    <a href="" class="basis-1/5 btn-register flex flex-row items-center justify-center	">Register Now</a>
+    
+    </div>
+
+</div>
+
+
+
+
 <style>
 @tailwind base;
 @tailwind components;
 @tailwind utilities;
 
+.nav
+{
+    height: 88px;
+    background: #0C0C0C;
+}
+.element
+{
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 20px;
+    line-height: 30px;
+    color: white;;
+}
+.logo-text
+{
+    font-family: Futura PT;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 32px;
+    line-height: 41px;
+    letter-spacing: 0.03em;
+    color: #FFFFFF;
+}
+.btn-register
+{
+    width: 165px;
+    height: 48px;
+    left: 931px;
+    top: 20px;
+    background: #F8CD57;
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 18px;
+    line-height: 27px;
+    color: #000000;
+    margin-right: 12%;
+  
+     
+}
 </style>
